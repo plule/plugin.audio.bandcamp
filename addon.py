af07a0b5d045288d40d46ea5773cea18cb7985a2
Pierre Lulé <pierre@lule.fr>
2013-04-05 21:43:08 +0200

    usable again

--- addon.py
+++ addon.py
@@ -1,4 +1,6 @@
-from xbmcswift2 import Plugin
+import sys,time,datetime
+from xbmcswift2 import Plugin,Module
+from xbmcswift2 import ListItem
 from xbmcswift2 import xbmc, xbmcgui
 import os
 from resources.lib.bandcamp import Bandcamp
@@ -35,17 +37,65 @@ def search(search_string):
     plugin.set_content('artists')
     items = [{
             'label': band['name']+' ('+band['subdomain']+')',
-            'path': plugin.url_for('show_band', 'band_id' = band['band_id'])
+            'path': plugin.url_for('show_band', band_id = band['band_id'])
             } for band in bands]
     return items
 
 @plugin.route('/band/<band_id>/')
 def show_band(band_id):
+    band = bc.band_info(band_id)
     albums = bc.discography(band_id)
     plugin.set_content('albums')
-    return []
+    items = []
+    for album in albums:
+        year = datetime.datetime.fromtimestamp(int(album['release_date'])).year
+        li = ListItem(
+            label = '{1} - {0}'.format(album['title'], year),
+            icon=album['large_art_url'],
+            thumbnail=album['large_art_url'],
+            path=plugin.url_for('show_album', album_id = album['album_id'])
+            )
+        infos = {
+            'year':year,
+            'album':album['title'],
+            'artist':band['name']
+            }
+        li.set_info('Music', infos)
+        items.append(li)
+    return items
+
+@plugin.route('/album/<album_id>/')
+def show_album(album_id):
+    plugin.set_content('songs')
+    album = bc.album_info(album_id)
+    band = bc.band_info(album['band_id'])
+    items = []
+    for track in album['tracks']:
+        art = track.get('large_art_url', album['large_art_url'])
+        li = ListItem(
+            label = '{0} - {1}'.format(track['number'], track['title']),
+            icon=art,
+            thumbnail=art,
+            path=plugin.url_for('play', track_id = track['track_id'])
+            )
+        li.set_is_playable(True)
+        infos = {
+            'title':track['title'],
+            'tracknumber':track['number'],
+            'duration':track['duration'],
+            'lyrics':track.get('lyrics',''),
+            'album':album['title'],
+            'artist':band['name']
+            }
+        li.set_info('Music', infos)
+        items.append(li)
+    return items
+
+@plugin.route('/track/<track_id>/')
+def play(track_id):
+    track = bc.track_info(track_id)
+    plugin.set_resolved_url(track['streaming_url'])
 
 if __name__ == '__main__':
-    bc = Bandcamp(open(keyFile,'r').read().rstrip())
-    print bc.search_band('brad hamers')
+    bc = Bandcamp(open(keyFile,'r').read().rstrip(), plugin)
     plugin.run()
--- resources/lib/bandcamp.py
+++ resources/lib/bandcamp.py
@@ -12,6 +12,7 @@ This file provides a simple python interface to bandcamp's api
 
 import urllib,urllib2
 import simplejson as json
+from xbmcswift2 import Module
 
 URL_API_BASE='http://api.bandcamp.com/api/'
 
@@ -46,15 +47,16 @@ class ApiError(Exception):
     pass
 
 class Bandcamp():
-    def __init__(self, key):
+    def __init__(self, key, plugin):
         self.key = key
-
-#band_cache = keydefaultdict(
-#    lambda band_id:call_api(MOD_BAND, FUNC_INFO, {'band_id':band_id}))
-#album_cache = keydefaultdict(
-#    lambda album_id: call_api(MOD_ALBUM, FUNC_INFO, {'album_id':album_id}))
-#track_cache = keydefaultdict(
-#    lambda track_id: call_api(MOD_TRACK, FUNC_INFO, {'track_id':track_id}))
+        self.api = plugin
+        self.bands = self.api.get_storage('bands', TTL = 60)
+        self.discographies = self.api.get_storage('discographies', TTL = 60)
+        self.albums = self.api.get_storage('albums', TTL = 60)
+        self.tracks = self.api.get_storage('tracks', TTL = 60)
+        
+    def get_module(self):
+        return self.api
 
     def call_api(self, module, function, params):
         url = URL_API_BASE + module + URL_API_VERSIONS[module] + function + '?' + 'key=' + self.key + '&' + urllib.urlencode(params)
@@ -68,7 +70,10 @@ class Bandcamp():
         return json.loads(urllib2.urlopen(url).read())
 
     def search_band(self, name):
-        return self.call_api(MOD_BAND, FUNC_SEARCH, {'name':name})['results']
+        res = self.call_api(MOD_BAND, FUNC_SEARCH, {'name':name})['results']
+        for band in res:
+            self.bands[band['band_id']] = band #TODO : doesn't work ?
+        return res
 
     def search_bands(self, names):
         return self.search_band(','.join(names)) #todo twelve max
@@ -76,18 +81,36 @@ class Bandcamp():
     def isalbum(self, thing):
         return 'album_id' in thing
 
-    def discography(self, bandid):
-        return filter(self.isalbum, self.call_api(MOD_BAND, FUNC_DISCOGRAPHY, {'band_id':band_id})['discography']) #TODO : remove filter
+    def discography(self, band_id):
+        band_id = int(band_id)
+        if band_id in self.discographies:
+            return self.discographies[band_id]
+        self.discographies[band_id] = filter(self.isalbum, self.call_api(MOD_BAND, FUNC_DISCOGRAPHY, {'band_id':band_id})['discography']) #TODO : remove filter
+        return self.discographies[band_id]
 
     def track_list(self, album_id):
         return call_api(MOD_ALBUM, FUNC_INFO, {'album_id':album_id})['tracks']
 
     def band_info(self, band_id):
-        return call_api(MOD_BAND, FUNC_INFO, {'band_id':band_id})
+        band_id = int(band_id)
+        if band_id in self.bands:
+            return self.bands[band_id]
+        self.bands[band_id] = self.call_api(MOD_BAND, FUNC_INFO, {'band_id':band_id})
+        return self.bands[band_id]
 
     def album_info(self, album_id):
-        album = call_api(MOD_ALBUM, FUNC_INFO, {'album_id':album_id})
+        album_id = int(album_id)
+        if album_id in self.albums:
+            return self.albums[album_id]
+        album = self.call_api(MOD_ALBUM, FUNC_INFO, {'album_id':album_id})
+        self.albums[album_id] = album
+        for track in album['tracks']:
+            self.tracks[track['track_id']] = track
         return album
 
     def track_info(self, track_id):
-        return call_api(MOD_TRACK, FUNC_INFO, {'track_id':track_id})
+        track_id = int(track_id)
+        if track_id in self.tracks:
+            return self.tracks[track_id]
+        self.tracks[track_id] = self.call_api(MOD_TRACK, FUNC_INFO, {'track_id':track_id})
+        return self.tracks[track_id]
